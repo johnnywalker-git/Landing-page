c1bc7bffd376860ceb0d9f1328af60a236bbe0f2
johnnywalker-git <91965220+johnnywalker-git@users.noreply.github.com>
2022-03-31 14:12:34 +0100

    Links for tracks working.

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 let trackImages = document.getElementById("background");
+let trackDownloads = document.getElementById("track-downloads")
 let mailingButton = document.getElementById("form-submit-event");
-let trackDownloads = [
 
-]
 const imgs = [
     "images/track-1.png",
     "images/track-2.png",
     "images/track-3.png"
 ]
 
-mailingButton.addEventListener('click', allowDownloads())
+const tracks = [
+    "https://github.com/johnnywalker-git/Landing-page/blob/johnnywalker-git-downloads/Time%20after%20Time%20-%20Johnny%20Walker.mp3?raw=true",
+    "https://github.com/johnnywalker-git/Landing-page/blob/johnnywalker-git-downloads/Intoxicated%20-%20Johnny%20Walker.mp3?raw=true",
+    "https://github.com/johnnywalker-git/Landing-page/blob/johnnywalker-git-downloads/Hallelujah%20Anyway%20-%20Johnny%20Walker%20&%20Luke%20Horsfield%20(Original%20Mix)%201.mp3?raw=true",
+]
 
-function allowDownloads(){
-imgs.forEach(img => trackImages.innerHTML+=`<img src=${img} class="track-background">`)
+// let trackId = tracks.forEach(track => console.log(track))
+
+
+
+function renderImage() {
+imgs.forEach(img => trackImages.innerHTML+=`<img src=${img} class="track-background"></a>`)
 }
+
+function renderTracks() {
+    tracks.forEach(track => trackDownloads.innerHTML+=`<a class="track-background" href="${track}">Download</a>`)
+    }
+
+console.log(renderImage())
+console.log(renderTracks())
